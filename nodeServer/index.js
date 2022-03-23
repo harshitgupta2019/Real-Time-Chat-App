910afa1874a436346286deb8aa1b693e2b9ccd53
harshitgupta2019 <63345282+harshitgupta2019@users.noreply.github.com>
2022-03-23 21:34:20 +0530

    Add files via upload

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,1 +1,28 @@
-
+//Node server
+const io = require('socket.io')(8000, {
+    cors: {
+      origin: '*',
+    }
+  });
+const users = {};
+// const cors=require("cors")
+// app.use(cors({
+//     origin:"http://127.0.0.1:5500",
+// }))
+io.on('connection', socket => {
+    //if any new users joins,lety others users connected to server know
+    socket.on('new-user-joined', name => {
+        console.log("New-user",name);
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+//if someone sends a message, broadcast it to other people
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', {message: message , name: users[socket.id]})
+    });
+    //if someone laeaves the chat,let others know
+    socket.on('disconnect', message => {
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
+})
